f6eb005d287b63fdacea9672ce2585e9110447b1
TanayB11 <tanaybiradar24@gmail.com>
2020-10-29 17:13:43 -0700

    Add cpp vim support

--- vim/snippets/usaco.cpp
+++ vim/snippets/usaco.cpp
@@ -1,15 +1,27 @@
-// Problem at URL
-
 #include <iostream>
 #include <cstdio>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-void setIO(string name = "") { // Replace with USACO problem name
+using ll = long long;
+using vi = vector<int>;
+#define pb push_back
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+void setIO(string name = "") { // Replace with USACO file name 
 	ios_base::sync_with_stdio(0); cin.tie(0);
-	freopen((name + ".in").c_str(), "r", stdin);
-	freopen((name + ".out").c_str(), "w", stdout);
+	freopen((name+".in").c_str(), "r", stdin);
+	freopen((name+".out").c_str(), "w", stdout);
 }
 
+// Main code
 int main() {
 	setIO();
 }
--- vimrc
+++ vimrc
@@ -72,6 +72,7 @@ autocmd bufnewfile *.java :-1read ~/.vim/snippets/apcs.java
 autocmd bufnewfile *.cpp :-1read ~/.vim/snippets/usaco.cpp
 autocmd FileType java nnoremap <silent> <C-e> :cd %:p:h <CR> :! javac %:t<CR> :! java %:t:r<CR>
 autocmd FileType python nnoremap <silent> <C-e> :cd %:p:h <CR> :! python3 %:t<CR>
+autocmd FileType cpp nnoremap <silent> <C-e> :cd %:p:h <CR> :! crun %:t:r<CR> :! cat %:t:r.out<CR>
 autocmd FileType markdown set spell
 
 
@@ -95,6 +96,7 @@ set laststatus=2
 set noshowmode	" hide default status line
 set shortmess+=I	" disable startup message
 set updatetime=300 " shorter update time makes it seem faster
+set shell=zsh\ -i
 
 
 " ------- 
